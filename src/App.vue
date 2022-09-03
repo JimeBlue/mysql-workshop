0f3661dcd1830e0cf7e51b4e7108fa9ea74adfa9
Jimena Miguez <jimeblau@gmail.com>
2022-09-03 11:47:12 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <registration />
   </div>
 </template>
+
+<script>
+import registration from "./views/registration.vue";
+export default {
+  components: { registration },
+};
+</script>
 
 <style>
 #app {
@@ -15,18 +18,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/views/registration.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-red-500 text-white">
+    <h1 class="text-red">I´m the registration page</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
